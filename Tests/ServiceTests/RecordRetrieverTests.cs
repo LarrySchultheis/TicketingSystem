6a89b58802ce99def3e721eb27adf90506a8d7ea
Schultheis <lks5334@psu.edu>
2019-11-06 18:34:27 -0500

    ported unit tests yayyyyyy

--- Tests/ServiceTests/RecordRetrieverTests.cs
+++ Tests/ServiceTests/RecordRetrieverTests.cs
@@ -1,21 +1,21 @@
-﻿//using NUnit.Framework;
-//using MVCApp.Services;
-//using MVCApp.Models;
-//using System.Collections.Generic;
+﻿using NUnit.Framework;
+using TicketingSystem.Services;
+using TicketingSystem.Models;
+using System.Collections.Generic;
 
-//namespace Prototype.Tests.ServiceTests
-//{
-//    [TestFixture]
-//    public class RecordRetrieverTests
-//    {
-//        [Test]
-//        public void RetrieveRecordsTest()
-//        {
-//            RecordRetriever rr = new RecordRetriever();
-//            var res = rr.RetrieveRecords();
-//            IEnumerable<TicketData> testModel = new List<TicketData>();
-//            Assert.IsNotNull(res);
-//            Assert.IsTrue(res.GetType() == testModel.GetType());
-//        }
-//    }
-//}
+namespace Prototype.Tests.ServiceTests
+{
+    [TestFixture]
+    public class RecordRetrieverTests
+    {
+        [Test]
+        public void RetrieveRecordsTest()
+        {
+            RecordRetriever rr = new RecordRetriever();
+            var res = rr.RetrieveRecords();
+            IEnumerable<TicketData> testModel = new List<TicketData>();
+            Assert.IsNotNull(res);
+            Assert.IsTrue(res.GetType() == testModel.GetType());
+        }
+    }
+}
