7f52254baf265147cd516b94052da4efbab6fec5
Larry Schultheis <lshults.21.117@gmail.com>
2020-03-07 01:03:38 -0500

    Got report generated through code

--- TicketingSystem/Services/ReportGenerator.cs
+++ TicketingSystem/Services/ReportGenerator.cs
@@ -8,12 +8,19 @@ using System.ServiceModel;
 using System.Threading.Tasks;
 using System.Net.Http;
 using Microsoft.ReportingServices.Interfaces;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Net.Http.Headers;
 
 namespace TicketingSystem.Services
 {
     public class ReportGenerator
     {
         protected static readonly string ssrsBaseURL = "http://localhost/reportserver";
+        protected static readonly string ssrsRestURL = "http://localhost/reports/api/v2.0";
+
 
 
         public List<TicketData> GenerateIncentveReport(DateTime startDate, DateTime endDate)
@@ -24,11 +31,12 @@ namespace TicketingSystem.Services
 
         public void GenerateReport (ReportInput reportData)
         {
+            HttpResponseMessage resp;
             if (reportData.ReportType == 0)
                 GenerateIncentveReport(reportData.StartDate, reportData.EndDate);
 
             else if (reportData.ReportType == 1)
-                GenerateLaborHoursByJob();//reportData.StartDate, reportData.EndDate);
+                GenerateLaborHoursByJob(reportData.StartDate, reportData.EndDate);//reportData.StartDate, reportData.EndDate);
 
             else if (reportData.ReportType == 2)
                 GenerateLaborHoursByJobAndEmployee(reportData.StartDate, reportData.EndDate);
@@ -40,95 +48,29 @@ namespace TicketingSystem.Services
             }
         }
 
-        public async void GenerateLaborHoursByJob()//DateTime startDate, DateTime endDate)
+        public async void GenerateLaborHoursByJob(DateTime startDate, DateTime endDate)//DateTime startDate, DateTime endDate)
         {
-            //List<LaborHoursByJob> data = new List<LaborHoursByJob>();
+            string start, end;
+            start = startDate.ToString("MM/dd/yyyy");
+            end = endDate.ToString("MM/dd/yyyy");
 
-            //using (var db = new TicketingSystemDBContext())
-            //{
-            //    var td = db.TicketData.Where(t => t.EntryDate >= startDate && t.EntryDate <= endDate).ToList();
-            //    foreach (TicketData t in td)
-            //    {
-            //        LaborHoursByJob lh = new LaborHoursByJob();
-            //        lh.Cases = t.CasesNum;
-            //        lh.Pallets = t.PalletNum;
-            //        lh.Job = db.JobType.Where(j => j.JobTypeId == t.JobTypeId).FirstOrDefault();
-            //        if (t.EndTime == null)
-            //        {
-            //            lh.TotalHours = DateTime.Now - t.StartTime;
-            //        }
-            //        else
-            //        {
-            //            lh.TotalHours = t.EndTime - t.StartTime;
-            //        }
-            //        data.Add(lh);
-            //    }
-            //}
+            HttpClientHandler handler = new HttpClientHandler
+            {
+                PreAuthenticate = true,
+                Credentials = new NetworkCredential("larry", "ls150682")
+            };
+            var client = new HttpClient();
 
-            //Call Report server endpoint with parameters
-            //serve file via binary
+            var resp = await client.GetAsync(ssrsBaseURL + "?/TicketingSystemReporting/LaborHoursByJob&rs:Format=csv&StartDate=" + start + "&EndDate=" + end);
 
-            //paramlist.Add(new ReportParameter(""))
-
-            //var x = 0;
-
-            //BasicHttpBinding rsBinding = new BasicHttpBinding();
-            //rsBinding.Security.Mode = BasicHttpSecurityMode.TransportCredentialOnly;
-            //rsBinding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Ntlm;
-
-            //EndpointAddress rsEndpointAddress = new EndpointAddress("http://localhost:80/reportserver/ReportService2010.asmx");
-            //ReportingService2010SoapClient rsClient = new ReportingService2010SoapClient(rsBinding, rsEndpointAddress);
-
-
-
-
-
-            //if (output.Status == TaskStatus.RanToCompletion && output.Result.Length > 0)
-            //{
-            //    foreach (CatalogItem item in output.Result)
-            //    {
-
-            //    }
-            //}
-
-            //BasicHttpBinding rsBinding = new BasicHttpBinding();
-            //rsBinding.Security.Mode = BasicHttpSecurityMode.TransportCredentialOnly;
-            //rsBinding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Ntlm;
-
-
-            //EndpointAddress rsEndpointAddress = new EndpointAddress("http://localhost:80/reportserver/ReportService2005.asmx");
-            //ReportExecutionServiceSoapClient rsc = new ReportExecutionServiceSoapClient(rsBinding, rsEndpointAddress);
-
-            //ServerInfoHeader serverInfoHeader;
-            //ExecutionInfo executionInfo;
-
-            HttpClient httpClient = new HttpClient();
-            httpClient.BaseAddress = new Uri(ssrsBaseURL);
-            
-            var resp = await httpClient.GetAsync(ssrsBaseURL + "/report");
-
+            var data = await resp.Content.ReadAsByteArrayAsync();
+            using (Stream file = File.OpenWrite("test.csv"))
+            {
+                file.Write(data, 0, data.Length);
+            }
 
             var y = 0;
-
-
-
         }
-
-        //private static async Task<CatalogItem[]> rsListChildren(String ItemPath, ReportingService2010SoapClient rsClient)
-        //{
-        //    TrustedUserHeader trustedUserHeader = new TrustedUserHeader();
-        //    ListChildrenResponse listChildrenResponse = null;
-        //    try
-        //    {
-        //        listChildrenResponse = await rsClient.ListChildrenAsync(trustedUserHeader, ItemPath, false);
-        //    }
-        //    catch (Exception exception)
-        //    {
-        //        Console.WriteLine(exception.Message + exception.StackTrace);
-        //        return new CatalogItem[0];
-        //    }
-        //    return listChildrenResponse.CatalogItems;
-        //}
 
         public void GenerateLaborHoursByJobAndEmployee(DateTime startDate, DateTime endDate)
         {
@@ -141,6 +83,15 @@ namespace TicketingSystem.Services
 
 
             return lh;
+        }
+
+        private byte[] ReadStream(Stream stream)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                stream.CopyTo(ms);
+                return ms.ToArray();
+            }
         }
     }
 }
